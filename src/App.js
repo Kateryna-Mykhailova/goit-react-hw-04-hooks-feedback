661a435b55d44977863548121a2ae32a8996844b
Kateryna-Mykhailova <katarrina.mk@gmail.com>
2021-11-08 11:23:51 +0100

    Update App

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import './App.css';
 import Section from '../src/components/Feedback/Section';
 import FeedbackOptions from '../src/components/Feedback/FeedbackOptions';
@@ -54,60 +54,3 @@ export default function App() {
     </div>
   );
 }
-
-// import React from 'react';
-// import './App.css';
-// import Section from '../src/components/Feedback/Section';
-// import FeedbackOptions from '../src/components/Feedback/FeedbackOptions';
-// import Statistics from '../src/components/Feedback/Statistics';
-// import Notification from './components/Feedback/Notification';
-// class App extends React.Component {
-//   state = {
-//     good: 0,
-//     neutral: 0,
-//     bad: 0,
-//   };
-
-//   handleIncrement = e => {
-//     console.log(e.target.name);
-//     this.setState(prevState => ({
-//       [e.target.name]: prevState[e.target.name] + 1,
-//     }));
-
-//   };
-
-//   countTotalFeedback = () => {
-
-//     return Object.values(this.state).reduce((acc, item) => (acc += item), 0);
-
-//   };
-
-//   countPositiveFeedbackPercentage = () => {
-
-//     return Math.round((this.state.good * 100) / this.countTotalFeedback());
-//   };
-
-//   render() {
-//     return (
-//       <div className="Feedback_container">
-//         <Section title="Please leave feedback">
-//           <FeedbackOptions onIncrementBad={this.handleIncrement} />
-//           {this.countTotalFeedback() ? (
-//             <Statistics
-//               good={this.state.good}
-//               neutral={this.state.neutral}
-//               bad={this.state.bad}
-//               total={this.countTotalFeedback()}
-//               positivePercentage={this.countPositiveFeedbackPercentage()}
-//             />
-//           ) : (
-//             <Notification message="No feedback given" />
-//           )}
-
-//         </Section>
-
-//       </div>
-//     );
-//   }
-// }
-// export default App;
